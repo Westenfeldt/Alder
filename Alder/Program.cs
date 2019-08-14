8b5f696c4825595efe31e3de5893a6df5669b3d2
Stefan Vestenfeldt <stefanwestenfeldt@gmail.com>
2019-08-14 12:52:45 +0200

    Add project files.

---
+++ Alder/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Alder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string age;
+            double age1;
+
+            Console.WriteLine("Angiv dit navn, derefter din alder");
+
+            name = Console.ReadLine();
+            age = Console.ReadLine();
+
+            age1 = float.Parse(age);
+
+            
+
+            if (age1 > 50)
+                {
+                    Console.WriteLine("Du er for gammel");
+                }
+                else if (age1 <= 50)
+                {
+                    Console.WriteLine("Du er godkendt");
+                }
+
+
+
+        }
+    }
+}
